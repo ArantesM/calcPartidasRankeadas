b8521a3312b04eaf2b6fb54aebb7f3c475a2cc82
ArantesM <michell.arantes@outlook.com.br>
2024-03-18 19:23:21 -0300

    Criado rank de heróis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+    public static String rank(String name, int win, int loss){
+        int result = win - loss;
+        String rank;
+
+        if(result <= 10){
+            rank = "Ferro";
+        }else if (result >= 10 && result <= 20) {
+            rank = "Bronze";
+        }else if (result > 20 && result <= 50) {
+            rank = "Prata";
+        }else if (result > 50 && result <= 80) {
+            rank = "Ouro";
+        }else if (result > 80 && result <= 90) {
+            rank = "Diamante";
+        }else if (result > 90 && result <= 100) {
+            rank = "Lendário";
+        } else {
+            rank = "Imortal";
+        }
+
+        return ("O herói " +name+ " tem de saldo: " +result+ "está no nível: " +rank);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.printf(rank("Superman", 121, 33));
+
+
     }
 }
